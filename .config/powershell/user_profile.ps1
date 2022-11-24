8a56fd3cee1d353005817a6371a2bdb478215109
Dan <undefinednulla@gmail.com>
2022-11-25 05:43:21 +0700

    dotfile o-m-p

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,1 +1,14 @@
-Import-Module -Name Terminal-Icons
+# Alias
+Set-Alias v nvim
+Set-Alias g git 
+
+# Import Module
+Import-Module Terminal-Icons
+oh-my-posh.exe init pwsh | Invoke-Expression
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-Scriptdirectory) 'dan.omp.json'
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -PredictionSource History
